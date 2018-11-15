8cb2704459367e9b677bbabd73ac2c230c6133db
franzsi <franzsi@gmail.com>
2018-11-15 20:09:59 +0100

    added comment

--- first_period/week03/3W4/06_Bunnies_02/main.cpp
+++ first_period/week03/3W4/06_Bunnies_02/main.cpp
@@ -4,6 +4,7 @@
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
 // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+// Odd - 2
 
 int bunnyEars (int bunnyOdd, int bunnyEven) {
     if (bunnyOdd < 1) {
