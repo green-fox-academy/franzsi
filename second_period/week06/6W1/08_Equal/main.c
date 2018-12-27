a07a2e95c80e065c2864c4617c7b5fc86832320b
franzsi <franzsi@gmail.com>
2018-12-27 01:52:26 +0100

    exit code problem

--- second_period/week06/6W1/08_Equal/main.c
+++ second_period/week06/6W1/08_Equal/main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int equal(int number1, int number2);
+
+int number1 = 0;
+int number2 = 0;
 
 int main()
 {
-    printf("Hello, World!\n");
-    return 0;
+    // Create a program which asks for two integers and stores them separatly
+    // Create a function which takes two numbers as parameters and
+    // returns 1 if they are equal and returns 0 otherwise
+
+    printf("Give me a number: \n");
+    scanf("%d", &number1);
+
+    printf("Give me an other number: \n");
+    scanf("%d", &number2);
 }
+    int equal(int number1, int number2)
+    {
+        if (number1 == number2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
