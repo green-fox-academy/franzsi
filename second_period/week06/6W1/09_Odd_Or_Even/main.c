7c53292444831f2562b740ac56a177f6531562a7
franzsi <franzsi@gmail.com>
2019-01-06 00:00:08 +0100

    return value

--- second_period/week06/6W1/09_Odd_Or_Even/main.c
+++ second_period/week06/6W1/09_Odd_Or_Even/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int isOdd(int number);
-
-int number = 0;
-
+int is_Odd(int number)
+{
+    if (number % 2 != 0) {
+        printf("This number is odd. \n");
+        return 1;
+    } else {
+        printf("This number is even. \n");
+        return 0;
+    }
+}
 int main()
 {
     // Create a program which asks for a number and stores it
@@ -12,17 +18,13 @@ int main()
     // returns 1 if that number is even and returns 0 otherwise
     // (in this case 0 is an even number)
 
+    int i;
     printf("Give me a number: \n");
-    scanf("%d", &number);
+    scanf("%d", &i);
+    printf("Your number is: %i \n" , i);
 
-    int isOdd(int number)
-    {
-        if (number % 2 == 1) {
-            printf("This number is odd \n");
-            return 1;
-        } else {
-            printf("This number is odd \n");
-            return 0;
-        }
-    }
+    is_Odd(i);
+
+    return 0;
+
 }
