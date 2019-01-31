0815a6ecfcd046e24baca0e35513e3bb0b57be14
franzsi <franzsi@gmail.com>
2019-01-31 12:22:59 +0100

    Image loading

--- third_period/OpenCV_Team2/Image_Loading/main.cpp
+++ third_period/OpenCV_Team2/Image_Loading/main.cpp
@@ -2,24 +2,21 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
-#include <string>
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
-    cv::String imageName( "../blabla.jpg" );
-    if( argc > 1)
-    {
+    cv::String imageName("../blabla.jpg");
+    if( argc > 1) {
         imageName = argv[1];
     }
     cv::Mat image;
-    image = cv::imread( imageName, cv::IMREAD_COLOR );
-    if( image.empty() )
-    {
-        std::cout <<  "Sorry baby, I can't find the image." << std::endl ;
+    image = cv::imread(imageName, cv::IMREAD_COLOR);
+    if(image.empty()) {
+        std::cout << "Sorry baby, I can't find the image." << std::endl;
         return -1;
     }
-    namedWindow( "Display window", cv::WND_PROP_FULLSCREEN  );
-    imshow( "Display window", image );
-    cv::waitKey(0); 
+    namedWindow("Display window", cv::WND_PROP_FULLSCREEN);
+    imshow("Display window", image);
+    cv::waitKey(0);
     return 0;
 }
